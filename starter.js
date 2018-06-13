f623878372f058a4ceb2772e151f453795a6829e
OfferZen-Maker <make@offerzen.com>
2018-06-13 13:42:12 +0200

    update
    update2

--- starter.js
+++ starter.js
@@ -1,27 +1,61 @@
 #!/usr/bin/env node
 
-const sphero = require("sphero")
+const awsIot = require('aws-iot-device-sdk');
+//const moment = require('moment'); // for DateTime formatting
+const username = 'arn:aws:iot:us-west-2:444146409335:thing/BullyBall' // TODO: replace this
 
-const spheroId = process.argv[2]
+const device = awsIot.device({
+   keyPath: '/home/pi/make/iot_robots/projects/sphero-cli/certificates/4606d6ca19-private.pem.key',
+  certPath: '/home/pi/make/iot_robots/projects/sphero-cli/certificates/4606d6ca19-certificate.pem.crt',
+    caPath: '/home/pi/make/iot_robots/projects/sphero-cli/certificates/ca-cert.pem',
+  clientId: `team-bully`,
+			host: 'a2yujzh40clf9c.iot.us-east-2.amazonaws.com'
+});
+
+
+const sphero = require("sphero")
+const spheroId = 'F5:77:55:BE:40:A2'
 const orb = sphero(spheroId)
 
 const streamData = require("./lib/dataStream")
-const stdin = process.openStdin()
+//const stdin = process.openStdin()
 
-stdin.addListener("data", function(data) {
+device.on('connect', () => {
+  console.log('Subscriber client connected to AWS IoT cloud.\n');
 
-	let command = "orb." + data.toString().trim()
+  device.subscribe('bullyball1');
+  // TODO subscribe to more topics here
+});
 
-	try {
-		let result = eval(command)
-		console.log("[EXECUTING]: " + result)
+device.on('message', (topic, payload) => {
 
-	} catch(error) {
-		console.log(error)
-	}
-})
+  let message = JSON.parse(payload.toString());
 
-orb.connect(() => {
+  switch (topic) {
+    case 'bullyball1':
+		 console.log(`Message received on topic "${topic}"\n`);
+		  drawPlus();
+		 break;
+
+		default:
+      console.log(`Message received on topic "${topic}"\n`)
+  }
+});
+
+// stdin.addListener("data", function(data) {
+
+// 	let command = "orb." + data.toString().trim()
+
+// 	try {
+// 		let result = eval(command)
+// 		console.log("[EXECUTING]: " + result)
+
+// 	} catch(error) {
+// 		console.log(error)
+// 	}
+// })
+
+ orb.connect(() => {
 
 	console.log("connected... waiting for input:")
 
@@ -53,8 +87,57 @@ orb.connect(() => {
 		streamData.motorsBackEmf(data)
 	})
 
-})
+// 	drawPlus();
+ })
 
 orb.disconnect(() => {
 
 });
+
+function drawPlus(){
+	orb.color("green").delay(1000).then(() => {
+  return forward1unit();
+}).then(() => {
+  return back1unit();
+}).then(() => {
+  return left1unit();
+}).then(() => {
+  return right1unit();
+}).then(() => {
+  return back1unit();
+}).then(() => {
+  return forward1unit();
+}).then(() => {
+  return right1unit();
+}).then(() => {
+  return left1unit();
+})
+}
+
+var heading;
+var baseunit = 50;
+function forward1unit(){
+	heading = 0;
+	return orb.roll(baseunit,0).delay(2000);
+}
+
+function back1unit() {
+	heading = 180;	
+	return orb.roll(baseunit,180).delay(2000)
+}
+
+function left1unit() {
+	heading = 270;
+	return orb.roll(baseunit,270).delay(2000);
+}
+
+function right1unit() {
+	heading = 90;
+	return orb.roll(baseunit,90).delay(2000);
+}
+
+
+
+
+
+
